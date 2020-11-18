78823c158bae1f1f5e85c560c472edb6ae1f86cd
puglisac <alan.c.puglisi@gmail.com>
2020-11-18 12:56:10 -0600

    finished section 4

---
+++ LearningProperties/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningProperties
+{
+    class Player
+    {
+        public bool IsAlive { get
+            {
+                return Health > 0;
+            }
+        }
+        public int Health { get; private set; } = 100;
+
+        public void Hit()
+        {
+            Random r = new Random();
+            Health -= r.Next(5, 50);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            do
+            {
+                player.Hit();
+                Console.WriteLine("Is player alive: " + player.IsAlive);
+            } while (player.IsAlive);
+        }
+    }
+}
